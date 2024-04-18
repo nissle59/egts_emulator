bd652646bda8b2b64752aeb112a2e685fcda77fc
Mikhaylov Nikita <nixncom@gmail.com>
2024-04-18 09:35:38 +0300

    Upd

--- main.py
+++ main.py
@@ -16,10 +16,13 @@ def coords_prepare(coords):
         coord['angle'] = random.randint(0, 359)
         if coords[i + 1]:
             coord_next = coords[i + 1]
+            coord['next_coord'] = coord_next
             coord['angle'] = int(math.atan2(coord_next['longitude'] - coord['longitude'],
                                             coord_next['latitude'] - coord['latitude']) * 180 / math.pi)
+            coord['angle'] = coord['angle'] - 180
             if coord['angle'] < 0:
                 coord['angle'] = 360 + coord['angle']
+
         coord['speed'] = random.randint(25, 90)
     return coords
 
@@ -43,6 +46,37 @@ while True:
         sock.connect(('46.50.138.139', 65521))
         #sock.connect(('10.8.0.1', 6000))
         #sock.connect(('127.0.0.1', 7777))
+
+        cmd1 = EGTStrack(deviceid="40614705", deviceimei="358480081523995")
+
+        message_b = cmd1.new_message()  # get message
+
+        print('CLT >> "{}"'.format(message_b.hex()))
+        sock.sendall(message_b)  # sends a message to the server
+        recv_b = sock.recv(256)  #
+        print('SRV >> "{}"'.format(recv_b.hex()))
+
+        i = 0
+        coords = coords_prepare(coords)
+        for coord in coords:
+            cmd1.add_service(16,
+                             long=coord['longitude'],
+                             lat=coord['latitude'],
+                             speed=coord['speed'],
+                             angle=coord['angle']
+                             )
+            message_b = cmd1.new_message()
+            print(f"Angle: {coord['angle']} now: long[{coord['longitude']}] lat[{coord['latitude']}, next: long[{coord.get('next_coord',{}).get('longitude', None)}] lat[{coord.get('next_coord',{}).get('latitude', None)}]")
+            print('CLT >> "{}"'.format(message_b.hex()))
+            sock.sendall(message_b)
+            recv_b = sock.recv(256)
+            print('SRV >> "{}"'.format(recv_b.hex()))
+            time.sleep(2)
+            i += 1
+
+        sock.close()
+
+
     except socket.timeout as msg:
         print("Timeouterror : %s" % msg)
         continue
@@ -55,40 +89,13 @@ while True:
     except socks.GeneralProxyError as msg:
         print("GeneralProxyError : %s" % msg)
         continue
-    break
+    finally:
+        try:
+            sock.close()
+        except:
+            pass
+    #break
 
-# Create a class and configure the device
-# cmd1 = EGTStrack(deviceid="40614705", deviceimei="358480081523999")
-#
-
-# cmd1 = EGTStrack(deviceid="0", deviceimei="40614705")
-cmd1 = EGTStrack(deviceid="40614705", deviceimei="358480081523995")
-
-message_b = cmd1.new_message()  # get message
-
-print('CLT >> "{}"'.format(message_b.hex()))
-sock.sendall(message_b)  # sends a message to the server
-recv_b = sock.recv(256)  #
-print('SRV >> "{}"'.format(recv_b.hex()))
-
-i = 0
-coords = coords_prepare(coords)
-for coord in coords:
-    cmd1.add_service(16,
-                     long=coord['longitude'],
-                     lat=coord['latitude'],
-                     speed=coord['speed'],
-                     angle=coord['angle']
-                     )
-    message_b = cmd1.new_message()
-    print('CLT >> "{}"'.format(message_b.hex()))
-    sock.sendall(message_b)
-    recv_b = sock.recv(256)
-    print('SRV >> "{}"'.format(recv_b.hex()))
-    time.sleep(2)
-    i += 1
-
-sock.close()
 
 if __name__ == "__main__":
     pass
