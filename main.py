5c726f8a6b4067d6bc9e73f08387bb3021b0095b
Mikhaylov Nikita <nixncom@gmail.com>
2024-05-20 11:59:49 +0300

    Upd

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ import service
 app = FastAPI()
 
 @app.get("/add")
-def add_imei(imei: str, route: str, background_tasks: BackgroundTasks):
-    background_tasks.add_task(service.add_imei, imei, route)
-    return {"imei": imei}
+def add_imei(id: str, route: str, background_tasks: BackgroundTasks):
+    background_tasks.add_task(service.add_imei, id, route)
+    return {"id": id, "route": route}
